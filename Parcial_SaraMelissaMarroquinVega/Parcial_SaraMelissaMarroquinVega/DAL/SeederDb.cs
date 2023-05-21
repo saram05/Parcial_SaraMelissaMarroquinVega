d8663a1bb951c00e2747eb3a4e396be3046f68af
Sara Melissa Marroquín Vega <sara.marroquin@inducascos.com>
2023-05-21 17:06:19 -0500

    New SeederDb to populate DB

---
+++ Parcial_SaraMelissaMarroquinVega/Parcial_SaraMelissaMarroquinVega/DAL/SeederDb.cs
@@ -0,0 +1,37 @@
+﻿using Parcial_SaraMelissaMarroquinVega.DAL.Entities;
+
+namespace Parcial_SaraMelissaMarroquinVega.DAL
+{
+    public class SeederDb
+    {
+        private readonly DataBaseContext _context;
+        public SeederDb(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync(); //Esta línea me ayuda a crear mi BD de forma automática
+            await PopulateTicketsAsync();
+
+            await _context.SaveChangesAsync();
+        }
+        private async Task PopulateTicketsAsync()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for (int i = 0; i < 50000; i++)
+                {
+                    _context.Tickets.Add(
+                        new Ticket
+                        {
+                            UseDate = null,
+                            IsUsed = false,
+                            EntranceGate = null
+                        });
+                }
+            }
+        }
+    }
+}
