eaf69cffe7066d7e55d84af9bd8ebd1d35d7a080
Sara Melissa Marroquín Vega <sara.marroquin@inducascos.com>
2023-05-21 20:25:10 -0500

    New Ticket Controller.

---
+++ Parcial_SaraMelissaMarroquinVega/Parcial_SaraMelissaMarroquinVega/Controllers/TicketsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parcial_SaraMelissaMarroquinVega.DAL;
+using Parcial_SaraMelissaMarroquinVega.DAL.Entities;
+
+namespace Parcial_SaraMelissaMarroquinVega.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly DataBaseContext _context;
+
+        public TicketsController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("Get")]
+        public async Task<ActionResult<IEnumerable<Ticket>>> GetTickets()
+        {
+            var tickets = await _context.Tickets.ToListAsync();
+
+            if (tickets == null) return NotFound();
+
+            return tickets;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("Get/{id}")]
+        public async Task<ActionResult<Ticket>> GetTicketById(Guid? id)
+        {
+            var ticket = await _context.Tickets.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (ticket == null) return NotFound();
+
+            return Ok(ticket);
+        }
+
+        [HttpPut, ActionName("Edit")]
+        [Route("Edit/{id}")]
+        public async Task<ActionResult> EditTicket(Guid? id, Ticket Ticket)
+        {
+            try
+            {
+                if (id != Ticket.Id) return NotFound("Ticket not found");
+
+                Ticket.UseDate = DateTime.Now;
+                Ticket.IsUsed = true;
+                Ticket.EntranceGate = Ticket.EntranceGate;
+
+                _context.Tickets.Update(Ticket);
+                await _context.SaveChangesAsync(); // Aquí es donde se hace el Update...
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe", Ticket.Id));
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
+
+            return Ok(Ticket);
+        }
+    }
+}
